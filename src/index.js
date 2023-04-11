a65311a5246b5f7c5b479e5fc4ec7561c81b18e2
Fatih <fatihcihan666@gmail.com>
2023-04-11 13:36:58 +0300

    Refactored

--- src/index.js
+++ src/index.js
@@ -5,33 +5,38 @@ import Collapse from "./components/Collapse";
 
 const App = () => {
     return (
-        <div>
-            <div className="card-group">
-
-                <Collapse href="collapseExample1">
-                    <Card cardTitle="Card 1"
-                        cardText="Lorem ipsum dolor 1"
-                        updatedTime="Last updated 1 min ago"
-                        image="https://picsum.photos/id/1/200/300"
-                    />
-                </Collapse>
-
-                <Collapse href="collapseExample2">
-                    <Card cardTitle="Card 2"
-                        cardText="Lorem ipsum dolor 2"
-                        updatedTime="Last updated 2 min ago"
-                        image="https://picsum.photos/id/10/200/300"
-                    />
-                </Collapse>
-
-                <Collapse href="collapseExample3">
-                    <Card cardTitle="Card 3"
-                        cardText="Lorem ipsum dolor 3"
-                        updatedTime="Last updated 3 min ago"
-                        image="https://picsum.photos/id/100/200/300"
-                    />
-                </Collapse>
-
+        <div className="container mt-2">
+            <div className="row">
+                <div className="card-group w-100">
+                    <div className="col">
+                        <Collapse href="collapseExample1">
+                            <Card
+                                // cardTitle="Card 1"
+                                cardText="Lorem ipsum dolor 1"
+                                updatedTime="Last updated 1 min ago"
+                                image="https://picsum.photos/id/1/200/300"
+                            />
+                        </Collapse>
+                    </div>
+                    <div className="col">
+                        <Collapse href="collapseExample2">
+                            <Card cardTitle="Card 2"
+                                cardText="Lorem ipsum dolor 2"
+                                updatedTime="Last updated 2 min ago"
+                                image="https://picsum.photos/id/2/200/300"
+                            />
+                        </Collapse>
+                    </div>
+                    <div className="col">
+                        <Collapse href="collapseExample3">
+                            <Card cardTitle="Card 3"
+                                cardText="Lorem ipsum dolor 3"
+                                updatedTime="Last updated 3 min ago"
+                                image="https://picsum.photos/id/3/200/300"
+                            />
+                        </Collapse>
+                    </div>
+                </div>
             </div>
         </div>
     )
