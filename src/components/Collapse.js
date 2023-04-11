e4db0711e63f0dd97e6947417dd2f2f911b7638f
Fatih <fatihcihan666@gmail.com>
2023-04-12 02:42:54 +0300

    Use of setstate, binding and the importance of arrow function

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -5,16 +5,29 @@ class Collapse extends React.Component {
         super();
 
         this.state = {
-            showContent: true
+            showContent: false
         }
+
+        // this.showMore = this.showMore.bind(this);
     }
+
+
+    showMore = () => {
+        // console.log(this);
+        this.setState({ showContent: !this.state.showContent })
+   
+    }
+
+    // showMore() {
+    //     // console.log(this);       undefined
+    // }  
 
     render() {
         return (
             <div>
-                <a className="btn btn-primary w-100" >
+                <button onClick={this.showMore} className="btn btn-primary w-100" >
                     Link with href
-                </a>
+                </button>
                 {
                     this.state.showContent ? (
                         <div className="collapse show">
