66e278e3c20233cfd2038ee6bfcbfb2d1ba1b1bb
Fatih <fatihcihan666@gmail.com>
2023-04-11 12:26:20 +0300

    Overview of child props and multiple props

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from './components/Card';
-
+import Collapse from "./components/Collapse";
 
 const App = () => {
     return (
         <div>
             <div className="card-group">
-                <Card cardTitle="Card 1" />
-                <Card cardTitle="Card 2" />
-                <Card cardTitle="Card 3" />
+
+                <Collapse href="collapseExample1">
+                    <Card cardTitle="Card 1"
+                        cardText="Lorem ipsum dolor 1"
+                        updatedTime="Last updated 1 min ago"
+                        image="https://picsum.photos/id/1/200/300"
+                    />
+                </Collapse>
+
+                <Collapse href="collapseExample2">
+                    <Card cardTitle="Card 2"
+                        cardText="Lorem ipsum dolor 2"
+                        updatedTime="Last updated 2 min ago"
+                        image="https://picsum.photos/id/10/200/300"
+                    />
+                </Collapse>
+
+                <Collapse href="collapseExample3">
+                    <Card cardTitle="Card 3"
+                        cardText="Lorem ipsum dolor 3"
+                        updatedTime="Last updated 3 min ago"
+                        image="https://picsum.photos/id/100/200/300"
+                    />
+                </Collapse>
+
             </div>
         </div>
     )
