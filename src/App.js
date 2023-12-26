876e7464321972c32e13eaa7a17caadec593c163
Fatih <fatihcihan666@gmail.com>
2023-12-27 02:09:17 +0300

    the card component is wrapped in the parent component collapse

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Card from './components/Card';
+import Collapse from './components/Collapse';
 
 function App() {
   return (
     <div className="App">
       <div className="card-group">
-        <Card cardTitle="card title 1"
-          cardText="Lorem ipsum text 1"
-          updatedTime="Last updated 1 min ago..."
-          image="https://picsum.photos/id/8/200/300" />
-        <Card cardTitle="card title 2"
-          cardText="Lorem ipsum text 2"
-          updatedTime="Last updated 2 min ago..."
-          image="https://picsum.photos/id/17/200/300" />
-
-        <Card cardTitle="card title 3"
-          cardText="Lorem ipsum text 3"
-          updatedTime="Last updated 3 min ago..."
-          image="https://picsum.photos/id/24/200/300" />
+        <Collapse>
+          <Card cardTitle="card title 1"
+            cardText="Lorem ipsum text 1"
+            updatedTime="Last updated 1 min ago..."
+            image="https://picsum.photos/id/8/200/300" />
+        </Collapse>
+        <Collapse>
+          <Card cardTitle="card title 2"
+            cardText="Lorem ipsum text 2"
+            updatedTime="Last updated 2 min ago..."
+            image="https://picsum.photos/id/17/200/300" />
+        </Collapse>
+        <Collapse>
+          <Card cardTitle="card title 3"
+            cardText="Lorem ipsum text 3"
+            updatedTime="Last updated 3 min ago..."
+            image="https://picsum.photos/id/24/200/300" />
+        </Collapse>
       </div>
     </div >
   );
