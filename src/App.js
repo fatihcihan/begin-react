43d5a08418addc72b6065fbf2436dad9436552a6
Fatih <fatihcihan666@gmail.com>
2023-12-27 01:41:46 +0300

    added image attribute to card component, codes edited

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ function App() {
   return (
     <div className="App">
       <div className="card-group">
-        <Card cardTitle="card title 1" cardText="Lorem ipsum text 1" updatedTime="Last updated 1 min ago..." />
-        <Card cardTitle="card title 2" cardText="Lorem ipsum text 2" updatedTime="Last updated 2 min ago..." />
-        <Card cardTitle="card title 3" cardText="Lorem ipsum text 3" updatedTime="Last updated 3 min ago..." />
+        <Card cardTitle="card title 1"
+          cardText="Lorem ipsum text 1"
+          updatedTime="Last updated 1 min ago..."
+          image="https://picsum.photos/id/8/200/300" />
+        <Card cardTitle="card title 2"
+          cardText="Lorem ipsum text 2"
+          updatedTime="Last updated 2 min ago..."
+          image="https://picsum.photos/id/17/200/300" />
+
+        <Card cardTitle="card title 3"
+          cardText="Lorem ipsum text 3"
+          updatedTime="Last updated 3 min ago..."
+          image="https://picsum.photos/id/24/200/300" />
       </div>
     </div >
   );
