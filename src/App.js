a496f954e533ed2f15afafa29af4ef6ffb8bc6bb
Fatih <fatihcihan666@gmail.com>
2023-12-26 21:12:47 +0300

    html content was moved to card components and props were passed

--- src/App.js
+++ src/App.js
@@ -5,26 +5,11 @@ function App() {
   return (
     <div className="App">
       <div className="card-group">
-        <Card />
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-
+        <Card cardTitle="card title 1" />
+        <Card cardTitle="card title 2" />
+        <Card cardTitle="card title 3" />
       </div>
-    </div>
+    </div >
   );
 }
 
