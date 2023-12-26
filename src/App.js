1d45bcf6793a478381f1d441b5638450889e2634
Fatih <fatihcihan666@gmail.com>
2023-12-27 01:25:40 +0300

    cardText and updatedTime attributes have been added to the card component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <div className="card-group">
-        <Card cardTitle="card title 1" />
-        <Card cardTitle="card title 2" />
-        <Card cardTitle="card title 3" />
+        <Card cardTitle="card title 1" cardText="Lorem ipsum text 1" updatedTime="Last updated 1 min ago..." />
+        <Card cardTitle="card title 2" cardText="Lorem ipsum text 2" updatedTime="Last updated 2 min ago..." />
+        <Card cardTitle="card title 3" cardText="Lorem ipsum text 3" updatedTime="Last updated 3 min ago..." />
       </div>
     </div >
   );
