3365d09331c2623a98d4f8422cb113476617b1a5
Fatih <fatihcihan666@gmail.com>
2023-12-26 19:12:58 +0300

    card component imported and called

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <div className="card-group">
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
+        <Card />
         <div className="card">
           <img src="..." className="card-img-top" alt="..." />
           <div className="card-body">
