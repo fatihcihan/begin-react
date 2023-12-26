c8703d90f0ae45d938de0c889a027b27d9d8e225
Fatih <fatihcihan666@gmail.com>
2023-12-27 02:40:10 +0300

    props captured and href value used

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Collapse = (props) => {
-    console.log(props.children, 'card component');
+    console.log(props.href, 'collapse props');
     return (
         <div>
-            <a className="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            <a className="btn btn-primary" data-bs-toggle="collapse" href={"#".concat(props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
                 Link with href
             </a>
-            <div className="collapse" id="collapseExample">
+            <div className="collapse" id={props.href}>
                 {props.children}
             </div>
         </div>
