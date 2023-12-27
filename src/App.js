344f7ed3844f6b133a9cfb296c4e35209bb274c2
Fatih <fatihcihan666@gmail.com>
2023-12-27 19:39:47 +0300

    cardTitle was deleted in the card component (for default props)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div className="card-group w-100 p-2">
           <div className="col px-2">
             <Collapse href="collapseExample1">
-              <Card cardTitle="card title 1"
+              <Card
                 cardText="Lorem ipsum text 1"
                 updatedTime="Last updated 1 min ago..."
                 image="https://picsum.photos/id/8/200/300" />
@@ -17,7 +17,8 @@ function App() {
           </div>
           <div className="col px-2">
             <Collapse href="collapseExample2">
-              <Card cardTitle="card title 2"
+              <Card
+                cardTitle="card title 2"
                 cardText="Lorem ipsum text 2"
                 updatedTime="Last updated 2 min ago..."
                 image="https://picsum.photos/id/17/200/300" />
@@ -25,7 +26,8 @@ function App() {
           </div>
           <div className="col px-2">
             <Collapse href="collapseExample3">
-              <Card cardTitle="card title 3"
+              <Card
+                cardTitle="card title 3"
                 cardText="Lorem ipsum text 3"
                 updatedTime="Last updated 3 min ago..."
                 image="https://picsum.photos/id/24/200/300" />
