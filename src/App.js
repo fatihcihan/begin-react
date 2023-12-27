40bfeef7ea0969bd7fb8fe22b08739bd7791086f
Fatih <fatihcihan666@gmail.com>
2023-12-27 12:27:16 +0300

    design improved (collapse components are wrapped with container, row and col)

--- src/App.js
+++ src/App.js
@@ -4,28 +4,36 @@ import Collapse from './components/Collapse';
 
 function App() {
   return (
-    <div className="App">
-      <div className="card-group">
-        <Collapse href="collapseExample1">
-          <Card cardTitle="card title 1"
-            cardText="Lorem ipsum text 1"
-            updatedTime="Last updated 1 min ago..."
-            image="https://picsum.photos/id/8/200/300" />
-        </Collapse>
-        <Collapse href="collapseExample2">
-          <Card cardTitle="card title 2"
-            cardText="Lorem ipsum text 2"
-            updatedTime="Last updated 2 min ago..."
-            image="https://picsum.photos/id/17/200/300" />
-        </Collapse>
-        <Collapse href="collapseExample3">
-          <Card cardTitle="card title 3"
-            cardText="Lorem ipsum text 3"
-            updatedTime="Last updated 3 min ago..."
-            image="https://picsum.photos/id/24/200/300" />
-        </Collapse>
+    <div className="container">
+      <div className="row">
+        <div className="card-group">
+          <div className="col">
+            <Collapse href="collapseExample1">
+              <Card cardTitle="card title 1"
+                cardText="Lorem ipsum text 1"
+                updatedTime="Last updated 1 min ago..."
+                image="https://picsum.photos/id/8/200/300" />
+            </Collapse>
+          </div>
+          <div className="col">
+            <Collapse href="collapseExample2">
+              <Card cardTitle="card title 2"
+                cardText="Lorem ipsum text 2"
+                updatedTime="Last updated 2 min ago..."
+                image="https://picsum.photos/id/17/200/300" />
+            </Collapse>
+          </div>
+          <div className="col">
+            <Collapse href="collapseExample3">
+              <Card cardTitle="card title 3"
+                cardText="Lorem ipsum text 3"
+                updatedTime="Last updated 3 min ago..."
+                image="https://picsum.photos/id/24/200/300" />
+            </Collapse>
+          </div>
+        </div>
       </div>
-    </div >
+    </div>
   );
 }
 
