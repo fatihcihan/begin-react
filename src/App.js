6eb2989b69bdb6d66ce6296b933f34575657e620
Fatih <fatihcihan666@gmail.com>
2023-12-27 18:21:56 +0300

    design improved (bootstrap classes added to divs)

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="card-group">
-          <div className="col">
+        <div className="card-group w-100 p-2">
+          <div className="col px-2">
             <Collapse href="collapseExample1">
               <Card cardTitle="card title 1"
                 cardText="Lorem ipsum text 1"
@@ -15,7 +15,7 @@ function App() {
                 image="https://picsum.photos/id/8/200/300" />
             </Collapse>
           </div>
-          <div className="col">
+          <div className="col px-2">
             <Collapse href="collapseExample2">
               <Card cardTitle="card title 2"
                 cardText="Lorem ipsum text 2"
@@ -23,7 +23,7 @@ function App() {
                 image="https://picsum.photos/id/17/200/300" />
             </Collapse>
           </div>
-          <div className="col">
+          <div className="col px-2">
             <Collapse href="collapseExample3">
               <Card cardTitle="card title 3"
                 cardText="Lorem ipsum text 3"
@@ -33,7 +33,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
