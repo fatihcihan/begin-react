4811d4ed0ab5de9453237d54ea9e24953498c926
Fatih <fatihcihan666@gmail.com>
2023-12-30 15:39:03 +0300

    class component converted to funcitonal component

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-// Functional component
-const Collapse = (props) => {
-    return (
-        <div>
-            <a className="btn btn-primary w-100" data-bs-toggle="collapse" href={"#".concat(props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
-                Link with href
-            </a>
-            <div className="collapse show" id={props.href}>
-                {props.children}
+// Class component
+class Collapse extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <a className="btn btn-primary w-100" data-bs-toggle="collapse" href={"#".concat(this.props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
+                    Link with href
+                </a>
+                <div className="collapse show" id={this.props.href}>
+                    {this.props.children}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default Collapse;
