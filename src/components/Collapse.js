48ad41097a3507502948b6a5c7c5da6aad575d74
Fatih <fatihcihan666@gmail.com>
2023-12-30 16:37:59 +0300

    the showContent property in state was changed to boolean, bootstrap features and unnecessary fields were removed

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -6,20 +6,23 @@ class Collapse extends React.Component {
     constructor() {
         super();
         this.state = {
-            showContent: 'test content'
+            showContent: true
         }
     };
 
     render() {
         return (
             <div>
-                <a className="btn btn-primary w-100" data-bs-toggle="collapse" href={"#".concat(this.props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
+                <a className="btn btn-primary w-100" >
                     Link with href
                 </a>
-                <div className="collapse show" id={this.props.href}>
-                    {this.props.children}
-                    {this.state.showContent}
-                </div>
+                {
+                    this.state.showContent ? (
+                        <div className="collapse show">
+                            {this.props.children}
+                        </div>
+                    ) : null
+                }
             </div>
         );
     }
