97aa70e4889b6f3c36bad22dc342187a21860c66
Fatih <fatihcihan666@gmail.com>
2024-01-02 12:57:14 +0300

    child component made dynamic with map function

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -42,12 +42,14 @@ class Collapse extends React.Component {
         return (
             <div>
                 <button className="btn btn-primary w-100" onClick={this.showMore}>
-                    {this.props.children.props.cardTitle}
+                    {/* {this.props.children.props.cardTitle} */}
+                    {React.Children.map(this.props.children, children => children.props.cardTitle)}
                 </button>
                 {
                     this.state.showContent ? (
                         <div className="collapse show">
-                            {this.props.children}
+                            {/* {this.props.children} */} 
+                            {React.Children.map(this.props.children, children => children)}
                         </div>
                     ) : null
                 }
